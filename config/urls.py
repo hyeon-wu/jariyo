680bb9b4b219daf7887e30f064027beeba3aab59
서현우 <shw1639@naver.com>
2021-10-31 20:27:35 +0900

    urls.py edit

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from pybo import views
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pybo/', views.index),
+    path('pybo/', include('pybo.urls')),
 ]
